1586dd15015233a1b8d4afd51a9aecedd02fee90
Juan01010 <jgonzalezzabaleta09@correo.unicordoba.edu.co>
2024-11-06 15:07:17 -0500

    mas

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 let currentSlide = 0;
+const slides = document.querySelectorAll('.slides');
+const slider = document.querySelector('.slider');
 
-function moveSlide(direction) {
-    const slides = document.querySelectorAll('.slides');
-    currentSlide = (currentSlide + direction + slides.length) % slides.length;
-    const slider = document.querySelector('.slider');
+function updateSlidePosition() {
     slider.style.transform = `translateX(${-currentSlide * 100}%)`;
 }
 
+function moveSlide(direction) {
+    currentSlide = (currentSlide + direction + slides.length) % slides.length;
+    updateSlidePosition();
+}
 
-  document.querySelectorAll('.dropdown-submenu').forEach(function(element) {
-    element.addEventListener('mouseover', function(e) {
-      let submenu = this.querySelector('.dropdown-menu');
-      if (submenu) {
-        submenu.style.display = 'block';
-      }
-    });
-    element.addEventListener('mouseout', function(e) {
-      let submenu = this.querySelector('.dropdown-menu');
-      if (submenu) {
-        submenu.style.display = 'none';
-      }
-    });
-  });
+let slideInterval = setInterval(() => moveSlide(1), 5000);
+
+slider.addEventListener('mouseover', () => clearInterval(slideInterval));
+slider.addEventListener('mouseout', () => {
+    slideInterval = setInterval(() => moveSlide(1), 5000);
+});
+
 
